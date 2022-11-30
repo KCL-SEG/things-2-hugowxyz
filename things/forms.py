00c88680bc84877ba2e4e7616ac54a18417c300b
hugowxyz1 <hung.phan_huy@kcl.ac.uk>
2022-11-30 12:49:53 +0000

    a

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
 
+from django import forms
+
 # Create your forms here.
+
+class ThingForm(forms.Form):
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(max_length=120, widget=forms.Textarea, required=False)
+    quantity = forms.IntegerField(widget=forms.NumberInput)
